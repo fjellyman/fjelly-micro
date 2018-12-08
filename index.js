2cd92af70ea28c7470e8b9045eb3b32734cc9653
Jordan Fjellman <jordan.fjellman@gmail.com>
2018-12-08 15:51:23 -0600

    Add environment variables config

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// configure Env variables
+require('dotenv').config();
+
 module.exports = (req, res) => {
-  res.end('Welcome to Micro')
+  res.end(`Welcome to Micro, env = ${ process.env.STAGE || 'default' }`)
 }
